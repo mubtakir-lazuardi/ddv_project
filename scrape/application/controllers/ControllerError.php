2ebe80d7b8c26772cf63657f044ab75bcccf1dcd
kodekeras <farhanscience@hotmail.com>
2018-10-17 16:47:33 +0700

    Code Igniter

---
+++ scrape/application/controllers/ControllerError.php
@@ -0,0 +1,15 @@
+<?php
+    
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class ControllerError extends CI_Controller {
+    
+        public function index()
+        {
+            echo "<marquee>ERROR-404</marquee>";
+        }
+    
+    }
+    
+    /* End of file Controllername.php */
+    
---
+++ scrape/application/views/content/dashboard.php
@@ -0,0 +1,90 @@
+
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+      <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+               <div style="position:relative">
+                 <h3 style="right:100%">150</h3>
+                 <b style="left:100%">Keyword</b>
+               </div>
+              </div>
+              <div class="icon">
+                <i class="fa fa-search"></i>
+              </div>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>  <sup style="font-size: 20px"></sup></h3>
+
+                <b>Link</b>
+                
+              </div>
+              <div class="icon">
+                <i class="fa fa-link"></i>
+              </div>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>da</h3>
+
+                <b>Email Tujuan</b>
+              </div>
+              <div class="icon">
+                <i class="fa fa-at"></i>
+              </div>
+            </div>
+          </div>
+          <!-- ./col -->
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>44</h3>
+
+                <b>kategori</b>
+              </div>
+              <div class="icon">
+                <i class="fa fa-pencil-square-o"></i>
+              </div>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
